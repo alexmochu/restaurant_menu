908936adfb0923dad929ae26e201e7fec91c34d4
alexmochu <mochualex4@gmail.com>
2019-09-22 19:06:25 +0300

    setup menu Html markup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import menuItems from './menuItems';
 
-function App() {
+class App extends React.Component {
+  render() {
+  console.log('Mayo', menuItems);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Restaurant Menu
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <input type="checkbox"/>
+              <span className="checkmark">Salad</span>
+                <ul>
+                  <li>
+                    <input type="checkbox"/>
+                    <span className="checkmark">Santa Fe</span>
+                  </li>
+                  <li>
+                    <input type="checkbox"/>
+                    <span className="checkmark">Greek</span>
+                  </li>
+                  <li>
+                    <input type="checkbox"/>
+                    <span className="checkmark">Asian</span>
+                  </li>
+                  <li>
+                    <span className="extras">You might also want:</span>
+                  </li>
+                  <li>
+                    <input type="checkbox"/>
+                    <span className="checkmark">Dressing</span>
+                    <ul>
+                      <li>
+                        <input type="checkbox"/>
+                        <span className="checkmark">Italian</span>
+                      </li>
+                      <li>
+                        <input type="checkbox"/>
+                        <span className="checkmark">Blue Cheese</span>
+                      </li>
+                      <li>
+                        <input type="checkbox"/>
+                        <span className="checkmark">Ranch</span>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <input type="checkbox"/>
+                    <span className="checkmark">Bread</span>
+                  </li>
+                </ul>
+            </li>
+            <li>
+              <input type="checkbox"/>
+              <span className="checkmark">Entree</span>
+            </li>
+            <li>
+              <input type="checkbox"/>
+              <span className="checkmark">Soup</span>
+              <ul>
+                <li>
+                  <input type="checkbox"/>
+                  <span className="checkmark">Minestrone</span>
+                </li>
+                <li>
+                  <input type="checkbox"/>
+                  <span className="checkmark">Hot and Sour</span>
+                </li>
+                <li>
+                  <input type="checkbox"/>
+                  <span className="checkmark">Miso</span>
+                </li>
+                <li>
+                  <span className="extras">You might also want:</span>
+                </li>
+                <li>
+                  <input type="checkbox"/>
+                  <span className="checkmark">Bread</span>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
+}
 }
 
 export default App;
